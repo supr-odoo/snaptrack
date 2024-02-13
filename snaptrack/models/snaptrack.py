7fcee3cc3ee1421522013b4295092a6a0912c9b5
supr-odoo <supr@odoo.com>
2024-02-13 18:15:26 +0530

    list and form view added

--- snaptrack/models/snaptrack.py
+++ snaptrack/models/snaptrack.py
@@ -6,7 +6,9 @@ class User(models.Model):
     _description = "this is description"
 
     name = fields.Char(string="Name")
+    description = fields.Text(string="Description")
     email = fields.Char(string="Email")
+    phone = fields.Char(string="Phone no")
     services = fields.Selection(
         string="Services",
         selection=[
@@ -20,3 +22,5 @@ class User(models.Model):
         selection=[("ph1", "Ankit"), ("ph2", "Aman"), ("ph3", "Ayan")],
     )
     prefered_date = fields.Date("Prefered Date")
+    active = fields.Boolean(string="Active", default=True)
+    last_seen = fields.Datetime(string="Last Seen", default=fields.Datetime.now)
